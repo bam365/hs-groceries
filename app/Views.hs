4b3ff54a29c5a143f7c2e3310c2bf3cf42ac4b23
Blake Mitchell <bam365@umsystem.edu>
2022-05-26 09:27:10 -0500

    clean views up a little

--- app/Views.hs
+++ app/Views.hs
@@ -14,7 +14,6 @@ import Lucid.Base (makeAttribute)
 
 import GroceriesLib
 
-
                            
 hxget_ = makeAttribute "hx-get"
 hxpost_ = makeAttribute "hx-post"
@@ -23,7 +22,6 @@ hxtarget_ = makeAttribute "hx-target"
 hxtrigger_ = makeAttribute "hx-trigger"
 hxswap_ = makeAttribute "hx-swap"
 
-
 page :: Html () -> Html ()
 page body = doctypehtml_ (head_ headT <> body_ body)
   where 
@@ -31,17 +29,6 @@ page body = doctypehtml_ (head_ headT <> body_ body)
         title_ "GroceryList" 
         link_ [ rel_ "stylesheet", href_ "/css/site.css" ] 
         script_ [ src_ "/js/htmx.min.js" , defer_ "" ] ("" :: Text)
-
-addGroceryForm :: Html ()
-addGroceryForm = form_ [ hxpost_ "/grocery" ] (groceryInput <> button)
-  where
-    groceryInput = label_ (do toHtml ("Grocery" :: Text) 
-                              input_ [class_ "add-grocery-input", name_ "name", type_ "text"])
-    button = button_ "Add"
-
-clearGottenForm :: Html ()
-clearGottenForm = form_ [ class_ "clear-gotten", hxpost_ "/clear-gotten",  hxswap_ "none" ] $
-    button_ "Clear"
 
 mainPage :: Html ()
 mainPage = div_ [ class_ "main" ] (title <> addGroceryForm <> lst <> clearGottenForm)
@@ -52,18 +39,42 @@ mainPage = div_ [ class_ "main" ] (title <> addGroceryForm <> lst <> clearGotten
                , hxtrigger_ "load,newGrocery from:body,clearedGroceries from:body" 
                ] mempty 
 
+addGroceryForm :: Html ()
+addGroceryForm = form_ [ hxpost_ "/grocery" ] (groceryInput <> button)
+  where
+    groceryInput = label_ (
+        do toHtml ("Grocery" :: Text) 
+           input_ 
+                [ class_ "add-grocery-input"
+                , name_ "name"
+                , type_ "text"
+                ])
+    button = button_ "Add"
+
+groceryList :: [Grocery] -> Html ()
+groceryList groceries = 
+    table_ [class_ "grocery-list"] (
+        tbody_ [ hxtarget_ "closest tr", hxswap_ "outerHTML" ] (
+            forM_ groceries renderGroceryRow))
+
 renderGroceryRow :: Grocery -> Html ()
 renderGroceryRow (Grocery groceryId name active) = tr_ (checkCell <> labelCell)
   where
     labelCell :: Html ()
     labelCell = td_ (toHtml name) 
     formInputs = do
-        input_ ([ class_ "active-chk", type_ "checkbox", name_ "active" ] <> [ checked_ | active ])
+        input_ ([ class_ "active-chk"
+                , type_ "checkbox"
+                , name_ "active" 
+                ] <> [ checked_ | active ])
         let idText = pack . show $ groceryId
         input_ [ type_ "hidden", name_ "id", value_ idText ]
     checkCell = td_ (form_ [hxpatch_ "/grocery", hxtrigger_ "change"] formInputs)
 
-groceryList :: [Grocery] -> Html ()
-groceryList groceries = 
-    table_ [class_ "grocery-list"] (tbody_ [ hxtarget_ "closest tr", hxswap_ "outerHTML" ] $ 
-        forM_ groceries renderGroceryRow)
+clearGottenForm :: Html ()
+clearGottenForm = 
+    form_
+        [ class_ "clear-gotten"
+        , hxpost_ "/clear-gotten"
+        , hxswap_ "none" 
+        ] (button_ "Clear")
